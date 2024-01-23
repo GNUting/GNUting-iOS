c8de0d8224172f856a09de2f3936e95834958c71
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-23 13:05:39 +0900

    기초 세팅

---
+++ GNUting/GNUting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GNUting
+//
+//  Created by 원동진 on 2024/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
