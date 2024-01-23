954b26b2faeb25fdae8fbe90b9567d0c51cff712
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-23 15:27:53 +0900

    #1: [Add] View/UI(1,2,3,4)

--- GNUting/GNUting/SceneDelegate.swift
+++ GNUting/GNUting/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainVC = ViewController()
+        let mainVC = UINavigationController.init(rootViewController: AppStartVC())
         window?.rootViewController = mainVC
         window?.makeKeyAndVisible()
     }
