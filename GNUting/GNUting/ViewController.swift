36f931d61aee7646c4e30da5bfb449b92e5f9fcf
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-01-23 14:16:56 +0900

    Font추가

--- GNUting/GNUting/ViewController.swift
+++ GNUting/GNUting/ViewController.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
     }
 
-
+    
 }
 
