895894e354f32e7716bbd8f5946c8f3a718b3c24
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-17 14:36:42 +0900

    [Add] TabBarController Related to #15

---
+++ GNUting/GNUting/RequestStatus/RequestStatusVC.swift
@@ -0,0 +1,28 @@
+//
+//  RequestStatusVC.swift
+//  GNUting
+//
+//  Created by 원동진 on 2/17/24.
+//
+
+import UIKit
+
+class RequestStatusVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        addSubViews()
+        setAutoLayout()
+    }
+}
+extension RequestStatusVC{
+    private func addSubViews() {
+        
+    }
+    private func setAutoLayout(){
+        
+    }
+    
+}
