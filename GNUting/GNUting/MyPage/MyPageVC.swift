13c94f2e6f28a15dc9653bf8c719f38953745f8e
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-17 14:36:42 +0900

    [Add] TabBarController Related to #15

---
+++ GNUting/GNUting/MyPage/MyPageVC.swift
@@ -0,0 +1,28 @@
+//
+//  MyPageVC.swift
+//  GNUting
+//
+//  Created by 원동진 on 2/17/24.
+//
+
+import UIKit
+import SnapKit
+class MyPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        addSubViews()
+        setAutoLayout()
+    }
+}
+
+extension MyPageVC{
+    private func addSubViews() {
+        
+    }
+    private func setAutoLayout(){
+        
+    }
+    
+}
