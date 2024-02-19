7da89a13655b9baa8581189cae59c8d87ca0b7d3
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-19 23:40:36 +0900

    #21 과팅 글쓰기-View/UI(1,2,3,4)

---
+++ GNUting/GNUting/Home/DateBoard/Wrtie/ViewComponent/MemberTableViewHeader.swift
@@ -0,0 +1,35 @@
+//
+//  MemberTableViewHeader.swift
+//  GNUting
+//
+//  Created by 원동진 on 2/19/24.
+//
+
+import UIKit
+
+class MemberTableViewHeader: UITableViewHeaderFooterView {
+    static let identi = "MemberTableViewHeaderoid"
+    private let memberLabel : UILabel = {
+        let label = UILabel()
+
+        label.font = UIFont(name: Pretendard.Medium.rawValue, size: 18)
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(memberLabel)
+        memberLabel.snp.makeConstraints { make in
+            make.top.bottom.right.equalToSuperview()
+            make.left.equalToSuperview().offset(Spacing.left)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public func setMemberLabelCount(memberCount : Int){
+        memberLabel.text = "멤버(\(memberCount))"
+    }
+}
---
+++ GNUting/GNUting/Home/DateBoard/Wrtie/WriteDateBoardVC.swift
@@ -0,0 +1,120 @@
+//
+//  WriteDateBoardVC.swift
+//  GNUting
+//
+//  Created by 원동진 on 2/19/24.
+//
+
+import UIKit
+
+class WriteDateBoardVC: UIViewController {
+    let textViewPlaceHolder = "내용을 입력해주세요."
+    let sampleData = ["짱짱맨(asd123) 컴퓨터과학과 | 21살 | 20학번|안녕하세요...이쁜이 구해요","짱짱맨(asd123) 컴퓨터과학과 | 21살 | 20학번|안녕하세요...이쁜이 구해요","짱짱맨(asd123) 컴퓨터과학과 | 21살 | 20학번|안녕하세요...이쁜이 구해요"]
+    private lazy var titleContetnView : WrtieTitleContentView = {
+        let customView = WrtieTitleContentView()
+        customView.contentTextView.delegate = self
+        customView.contentTextView.text = textViewPlaceHolder
+        
+        return customView
+    }()
+    private lazy var memberTableView : UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.register(MemberTableViewCell.self, forCellReuseIdentifier: MemberTableViewCell.identi)
+        tableView.register(MemBerAddTableViewCell.self, forCellReuseIdentifier: MemBerAddTableViewCell.identi)
+        tableView.register(MemberTableViewHeader.self, forHeaderFooterViewReuseIdentifier: MemberTableViewHeader.identi)
+        return tableView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        addSubViews()
+        setAutoLayout()
+        setNavigationBar()
+        setTableView()
+    }
+    
+
+}
+extension WriteDateBoardVC{
+    private func setTableView(){
+        memberTableView.delegate = self
+        memberTableView.dataSource = self
+    }
+    private func setNavigationBar(){
+        let backButton = BackButton()
+        backButton.setConfigure(text: "홈")
+        backButton.addTarget(self, action: #selector(popButtonTap), for: .touchUpInside)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        self.navigationItem.title = "글쓰기"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: Pretendard.SemiBold.rawValue, size: 18)!]
+//        let rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "SearchImg"), style: .plain, target: self, action: #selector(tapSearchButton))
+//        rightBarButtonItem.tintColor = UIColor(named: "IconColor")
+//        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    private func addSubViews() {
+        view.addSubViews([titleContetnView,memberTableView])
+    }
+    private func setAutoLayout(){
+        titleContetnView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(Spacing.top)
+            make.left.equalToSuperview().offset(Spacing.left)
+            make.right.equalToSuperview().offset(Spacing.right)
+//            make.height.equalTo(self.view.frame.height).dividedBy(3)
+        }
+        memberTableView.snp.makeConstraints { make in
+            make.top.equalTo(titleContetnView.snp.bottom).offset(Spacing.top)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(2)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+    
+}
+extension WriteDateBoardVC : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if sampleData.count == 0{
+            return 1
+        }else {
+            return sampleData.count + 1
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if indexPath.row < sampleData.count{
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MemberTableViewCell.identi, for: indexPath) as? MemberTableViewCell else { return UITableViewCell()}
+            cell.setContentLabel(model: sampleData[indexPath.row])
+            return cell
+        }else{
+            guard let addCell = tableView.dequeueReusableCell(withIdentifier: MemBerAddTableViewCell.identi, for: indexPath) as? MemBerAddTableViewCell else { return UITableViewCell()}
+            return addCell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: MemberTableViewHeader.identi) as? MemberTableViewHeader else {return UIView()}
+        headerView.setMemberLabelCount(memberCount: sampleData.count)
+        return headerView
+    }
+
+}
+extension WriteDateBoardVC : UITextViewDelegate{
+    func textViewDidBeginEditing(_ textView: UITextView) {
+           if textView.text == textViewPlaceHolder {
+               textView.text = nil
+               textView.textColor = .black
+           }
+       }
+
+       func textViewDidEndEditing(_ textView: UITextView) {
+           if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+               textView.text = textViewPlaceHolder
+               textView.textColor = UIColor(named: "9F9F9F")
+           }
+       }
+}
+
