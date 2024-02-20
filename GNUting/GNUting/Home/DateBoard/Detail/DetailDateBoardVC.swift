0d0b95d78413ef019085ca09a7443249bb2a840b
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-20 17:34:34 +0900

    #23: [Feat] Action(1)

---
+++ GNUting/GNUting/Home/DateBoard/Detail/DetailDateBoardVC.swift
@@ -0,0 +1,18 @@
+//
+//  DetailDateBoardVC.swift
+//  GNUting
+//
+//  Created by 원동진 on 2/20/24.
+//
+
+import UIKit
+
+class DetailDateBoardVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
