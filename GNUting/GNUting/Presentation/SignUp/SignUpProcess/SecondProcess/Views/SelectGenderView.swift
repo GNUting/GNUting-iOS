61ff672f19c909b4eb7f1c705f2bd65bc310e6d2
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-06-26 15:02:27 +0900

    [Refactor] #102 - SelectGenderView 폴더위치변경, Mark 추가, 코드 컨벤션

--- GNUting/GNUting/Presentation/SignUp/SignUpProcess/SecondProcess/Views/SelectGenderView.swift
+++ GNUting/GNUting/Presentation/SignUp/SignUpProcess/SecondProcess/Views/SelectGenderView.swift
@@ -5,26 +5,37 @@
 //  Created by 원동진 on 2/8/24.
 //
 
-import Foundation
+// MARK: - 회원가입 (2) 성별 선택 View
+
 import UIKit
-class SelectGenderView : UIView {
-    var selectedGender : Int = 0 // 0 : 남자 , 1:여자
-    private let typeLabel : UILabel = {
-        let uiLabel = UILabel()
-        uiLabel.text = "성별"
-        uiLabel.font = Pretendard.semiBold(size: 14)
-        return uiLabel
+
+class SelectGenderView: UIView {
+    
+    // MARK: - Properties
+    
+    var selectedGender: Int = 0 // 0 : 남자 , 1:여자
+
+    // MARK: - SubViews
+    
+    private let typeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "성별"
+        label.font = Pretendard.semiBold(size: 14)
+        
+        return label
     }()
-    private lazy var buttonStackView : UIStackView = {
+    
+    private lazy var buttonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.spacing = 15
         stackView.distribution = .fillEqually
-        return stackView
         
+        return stackView
     }()
-    private lazy var manTypeButton : UIButton = {
+    
+    private lazy var manTypeButton: UIButton = {
         var config = UIButton.Configuration.plain()
         config.attributedTitle = AttributedString("남", attributes: AttributeContainer([NSAttributedString.Key.font: Pretendard.medium(size: 16) ?? .systemFont(ofSize: 16)]))
         config.baseForegroundColor = UIColor(hexCode: "767676")
@@ -38,9 +49,11 @@ class SelectGenderView : UIView {
         button.layer.borderWidth = 1
         button.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
         button.tag = 0
+        
         return button
     }()
-    private lazy var girlTypeButton : UIButton = {
+    
+    private lazy var girlTypeButton: UIButton = {
         var config = UIButton.Configuration.plain()
         config.attributedTitle = AttributedString("여", attributes: AttributeContainer([NSAttributedString.Key.font: Pretendard.medium(size: 16) ?? .systemFont(ofSize: 16)]))
         config.baseForegroundColor = UIColor(hexCode: "767676")
@@ -55,46 +68,60 @@ class SelectGenderView : UIView {
         button.layer.borderWidth = 1
         button.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
         button.tag = 1
+        
         return button
     }()
+    
+    // MARK: - init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        
+        setAddSubViews()
+        setAutoLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+// MARK: - Layout Helpers
+
 extension SelectGenderView {
-    private func configure(){
+    private func setAddSubViews() {
         self.addSubViews([typeLabel,buttonStackView])
+        
         buttonStackView.addStackSubViews([manTypeButton,girlTypeButton])
-        
-        
+    }
+    
+    private func setAutoLayout() {
         typeLabel.snp.makeConstraints { make in
             make.top.left.right.equalToSuperview()
         }
+        
         buttonStackView.snp.makeConstraints { make in
             make.top.equalTo(typeLabel.snp.bottom).offset(14)
             make.left.right.equalToSuperview()
             make.bottom.equalToSuperview().offset(-14)
         }
     }
-        
-    public func selectButton(isSelcted : Bool,tag : Int){
-        
-    }
-    @objc func tapButton(_ sender : UIButton){
+}
+
+// MARK: - Action
+
+extension SelectGenderView {
+    @objc func tapButton(_ sender : UIButton) {
         selectedGender = sender.tag
-        if sender.tag  == 0{
+        if sender.tag == 0{
             isSelctedButton(text: "남", isSelcted: true, button: manTypeButton)
             isSelctedButton(text: "여", isSelcted: false, button: girlTypeButton)
-        }else{
+        } else {
             isSelctedButton(text: "여", isSelcted: true, button: girlTypeButton)
             isSelctedButton(text: "남", isSelcted: false, button: manTypeButton)
         }
     }
+    
     func isSelctedButton(text: String,isSelcted : Bool,button : UIButton){
         if isSelcted{
             var config = UIButton.Configuration.plain()
@@ -108,7 +135,7 @@ extension SelectGenderView {
             button.layer.cornerRadius = 10
             button.layer.masksToBounds = true
             button.layer.borderWidth = 1
-        }else{
+        } else {
             var config = UIButton.Configuration.plain()
             config.attributedTitle = AttributedString("\(text)", attributes: AttributeContainer([NSAttributedString.Key.font: Pretendard.medium(size: 16) ?? .systemFont(ofSize: 16)]))
             config.baseForegroundColor = UIColor(hexCode: "767676")
@@ -121,10 +148,10 @@ extension SelectGenderView {
             button.layer.masksToBounds = true
             button.layer.borderWidth = 1
         }
-       
     }
 }
 
+// MARK: - Method public
 
 extension SelectGenderView {
     func getSelectedGender() -> String{
