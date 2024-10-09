b8dbb1f272ff828d0cc5f9dc2bc14d85d65a57c3
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2024-10-09 17:53:16 +0900

    [HotFix] - 채팅 리스트 디자인 수정

--- GNUting/GNUting/Presentation/Chat/VC/ChatRoomListVC.swift
+++ GNUting/GNUting/Presentation/Chat/VC/ChatRoomListVC.swift
@@ -129,7 +129,7 @@ extension ChatRoomListVC{
     
     private func checkOneMatching(userListCount: Int, studentID: String?, department: String?) -> String? {
         if userListCount == 1 {
-            return "\(studentID ?? "")|\(department ?? "학과")"
+            return "\(studentID ?? "") | \(department ?? "학과")"
         } else {
             return nil
         }
