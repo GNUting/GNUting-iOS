b5ec7b30a61a74b7bb1aa02ce0ef72aa55fb53b7
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2024-10-24 18:00:29 +0900

    [Chore] #134 - DateBoard/Report/VC Report VC Mark 추가 및 코드 컨벤션 , 정적인 String값 Strings 파일로 분리 후 관리

--- GNUting/GNUting/Presentation/Main/DateBoard/Report/VC/ReportVC.swift
+++ GNUting/GNUting/Presentation/Main/DateBoard/Report/VC/ReportVC.swift
@@ -5,41 +5,42 @@
 //  Created by 원동진 on 2/23/24.
 //
 
+// MARK: - 사용자 및 게시글 신고하기 ViewController
+
 import UIKit
 
 class ReportVC: BaseViewController {
+    
+    // MARK: - Properties
+    
     var userNickname: String = ""
-    let textViewPlaceHolder = "기타 사유를 입력해주세요."
+    let textViewPlaceHolder = Strings.Report.textViewPlaceHolder
     var boardID: Int = 0
-    var tag : Int = 0
-    private let explainLabel : UILabel = {
-        let fullText = """
-신고하기 전에 잠깐!
-
-이 글이 운영진에 의해 삭제되어야 마땅하다고 생각된다면 신고해주세요!
-이용규칙에 위배되는 글을 여러 차례 계시하여 신고를 많이 받은 회원은 제한 조취가 취해집니다.
-
-신고는 사용자의 반대 의견을 표시하는 것이 아닙니다.
-사용자의 신고가 건전하고 올바른 지누팅 문화를 만듭니다.
-허위 신고의 경우 신고자가 제재를 받을 수 있습니다.
-"""
+    var tag: Int = 0
+    
+    // MARK: - SubViews
+    
+    private let explainLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
         label.font = Pretendard.medium(size: 9)
         label.numberOfLines = 0
-        label.text = fullText
-        label.setRangeTextFont(fullText: fullText, range: "신고하기 전에 잠깐!", font: Pretendard.bold(size: 12) ?? .systemFont(ofSize: 12))
+        label.text = Strings.Report.explain
+        label.setRangeTextFont(fullText: Strings.Report.explain, range: "신고하기 전에 잠깐!", font: Pretendard.bold(size: 12) ?? .systemFont(ofSize: 12))
+        
         return label
     }()
-    private lazy var reportReasonView : ReportReasonView = {
+    
+    private lazy var reportReasonView: ReportReasonView = {
         let view = ReportReasonView()
         view.buttonTagClosure = { selectedTag in
             self.tag = selectedTag
         }
+        
         return view
     }()
     
-    private lazy var OtherReasonTextView : UITextView = {
+    private lazy var OtherReasonTextView: UITextView = {
         let textView = UITextView()
         textView.text = textViewPlaceHolder
         textView.textColor = UIColor(hexCode: "9F9F9F")
@@ -50,18 +51,21 @@ class ReportVC: BaseViewController {
         textView.layer.borderColor = UIColor(hexCode: "E0E0DF").cgColor
         textView.layer.masksToBounds = true
         textView.delegate = self
+        
         return textView
     }()
     
-    private lazy var bottomButtonStackView : UIStackView = {
+    private lazy var bottomButtonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.spacing = 12
+        
         return stackView
     }()
-    private lazy var cancelButton : UIButton = {
+    
+    private lazy var cancelButton: UIButton = {
         var config = UIButton.Configuration.plain()
         config.contentInsets = NSDirectionalEdgeInsets(top: 18, leading: 30, bottom: 18, trailing: 30)
         config.attributedTitle = AttributedString("취소하기", attributes: AttributeContainer([NSAttributedString.Key.font: Pretendard.bold(size: 14) ?? .boldSystemFont(ofSize: 14)]))
@@ -76,7 +80,8 @@ class ReportVC: BaseViewController {
         
         return button
     }()
-    private lazy var reportButton : ThrottleButton = {
+    
+    private lazy var reportButton: ThrottleButton = {
         var config = UIButton.Configuration.plain()
         config.contentInsets = NSDirectionalEdgeInsets(top: 18, leading: 30, bottom: 18, trailing: 30)
         config.attributedTitle = AttributedString("신고하기", attributes: AttributeContainer([NSAttributedString.Key.font: Pretendard.bold(size: 14) ?? .boldSystemFont(ofSize: 14)]))
@@ -89,32 +94,30 @@ class ReportVC: BaseViewController {
         button.throttle(delay: 3) { _ in
             self.tapReportButton()
         }
-//        button.addTarget(self, action: #selector(tapReportButton), for: .touchUpInside)
-        
         
         return button
     }()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addSubViews()
         setAutoLayout()
-        setNavigationBar()
-        
-    }
-}
-extension ReportVC{
-    
-    private func setNavigationBar(){
         setNavigationBarPresentType(title: "신고하기")
     }
-    
 }
-extension ReportVC{
+
+extension ReportVC {
+    
+    // MARK: - Layout Helpers
+    
     private func addSubViews() {
         view.addSubViews([explainLabel, reportReasonView, OtherReasonTextView, bottomButtonStackView])
         bottomButtonStackView.addStackSubViews([cancelButton, reportButton])
     }
+    
     private func setAutoLayout() {
         explainLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(Spacing.top)
@@ -139,24 +142,9 @@ extension ReportVC{
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-15)
         }
     }
-    
 }
-extension ReportVC : UITextViewDelegate{
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.text == textViewPlaceHolder {
-            textView.text = nil
-            textView.textColor = .black
-            
-        }
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-            textView.text = textViewPlaceHolder
-            textView.textColor = UIColor(hexCode: "9F9F9F")
-        }
-    }
-}
+
+// MARK: - Private Method
 
 extension ReportVC {
     private func tapReportButton() {
@@ -216,6 +204,24 @@ extension ReportVC {
             }
         }
     }
+}
 
+// MARK: - Delegate
+
+extension ReportVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == textViewPlaceHolder {
+            textView.text = nil
+            textView.textColor = .black
+            
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = textViewPlaceHolder
+            textView.textColor = UIColor(hexCode: "9F9F9F")
+        }
+    }
 }
 
