c1c311a0ddb97962638462f736d70c999213d5ff
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2024-10-30 00:10:46 +0900

    [Chore]  #134 - DateBoard/Write+Update/VC WriteDateBoardVC - 접근제한자 ,Mark, 설명 주석 추가, 중복 제거, 삼항 연산자 사용, 코드 정리, 사용하지 않는 함수 제거, 글쓰기 내용 비어있을때 Alert출력

--- GNUting/GNUting/Presentation/Main/DateBoard/Wrtie+Update/VC/WriteDateBoardVC.swift
+++ GNUting/GNUting/Presentation/Main/DateBoard/Wrtie+Update/VC/WriteDateBoardVC.swift
@@ -5,27 +5,32 @@
 //  Created by 원동진 on 2/19/24.
 //
 
+// MARK: - 게시글 작성 ViewController
+
 import UIKit
 
-class WriteDateBoardVC: BaseViewController {
-    let textViewPlaceHolder = "내용을 입력해주세요."
-    var writeEnable : Bool = false
-    var addMemberDataList: [UserInfosModel] = [] {
+final class WriteDateBoardVC: BaseViewController {
+    
+    // MARK: - Properties
+    
+    private var writeEnable: Bool = false
+    private var addMemberDataList: [UserInfosModel] = [] {
         didSet {
             memberTableView.reloadData()
         }
     }
-  
-    private lazy var titleContentView : WrtieUpdatePostTextView = {
-        let customView = WrtieUpdatePostTextView()
-        customView.contentTextView.delegate = self
-        customView.contentTextView.text = textViewPlaceHolder
+    
+    // MARK: - SubViews
+
+    private lazy var titleContentView: WriteUpdatePostTextView = {
+        let view = WriteUpdatePostTextView()
+        view.wrtieUpdatePostTextViewDelegate = self
         
-        return customView
+        return view
     }()
-    private lazy var memberTableView : UITableView = {
+    
+    private lazy var memberTableView: UITableView = {
         let tableView = UITableView()
-        
         tableView.sectionHeaderTopPadding = 0
         tableView.separatorStyle = .none
         tableView.bounces = false
@@ -35,125 +40,108 @@ class WriteDateBoardVC: BaseViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.showsVerticalScrollIndicator = false
+        
         return tableView
     }()
+    
     private lazy var completedButton: ThrottleButton = {
        let button = ThrottleButton()
         button.setTitle("완료", for: .normal)
         button.isEnabled = false
         button.titleLabel?.font = Pretendard.medium(size: 18)
         button.setTitleColor(UIColor(named: "SecondaryColor"), for: .normal)
-      
+        button.throttle(delay: 3) { _ in
+            self.completedButtonAction()
+        }
         
         return button
     }()
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
-    }
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getUserData()
-        addSubViews()
+        setAddSubViews()
         setAutoLayout()
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         setNavigationBar()
-        setCompletedButton()
+        getUserDataAPI()
     }
-    
-
 }
-extension WriteDateBoardVC{
-    private func setNavigationBar(){
-        setNavigationBar(title: "글쓰기")
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: completedButton)
-    }
-    private func setCompletedButton() {
-        completedButton.throttle(delay: 3) { _ in
-            if self.titleContentView.getTitleTextFieldText()?.count == 0 {
-                self.showAlert(message: "글 제목 또는 내용을 채워주세요.")
-            } else if self.titleContentView.getContentTextViewText().count == 0{
-                self.showAlert(message: "글 제목 또는 내용을 채워주세요.")
-            } else {
-                self.tapCompletedButton()
-            }
-        }
-    }
-    private func addSubViews() {
+
+extension WriteDateBoardVC {
+    
+    // MARK: - Layout Helpers
+    
+    private func setAddSubViews() {
         view.addSubViews([titleContentView, memberTableView])
     }
-    private func setAutoLayout(){
+    
+    private func setAutoLayout() {
         titleContentView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(Spacing.top)
-            make.left.equalToSuperview().inset(Spacing.size25)
-            make.right.equalToSuperview().offset(Spacing.right)
+            make.left.right.equalToSuperview().inset(Spacing.size25)
         }
         
         memberTableView.snp.makeConstraints { make in
             make.top.equalTo(titleContentView.snp.bottom).offset(Spacing.top)
             make.left.right.equalToSuperview().inset(Spacing.size27)
-            
-            make.height.equalToSuperview().dividedBy(2)
             make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+            make.height.equalToSuperview().dividedBy(2)
         }
     }
     
-}
+    // MARK: - SetView
+    
+    private func setNavigationBar() {
+        setNavigationBar(title: "글쓰기")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: completedButton)
+    }
 
-// MARK: - DataSource
-
-extension WriteDateBoardVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 {
-            let vc = SearchAddMemberVC()
-            
-            vc.searchAddMemberVCDelegate = self
-            vc.setProperties(pushRequestChatVC: false, addMemberInfos: addMemberDataList)
-            presentViewController(viewController: vc)
+    private func addMemberID() -> [UserIDList] {
+        var joinMemberIDList: [UserIDList] = []
+        
+        for userData in addMemberDataList {
+            joinMemberIDList.append(UserIDList(id: userData.id))
+        }
+        return joinMemberIDList
+    }
+    
+    // MARK: - API
+    
+    private func getUserDataAPI() {
+        APIGetManager.shared.getUserData { userData,response  in
+            self.errorHandling(response: response)
+            guard let userData = userData?.result else { return }
+            self.addMemberDataList.append(UserInfosModel(id: userData.id,
+                                                         name: userData.nickname,
+                                                         gender: userData.gender,
+                                                         age: userData.age,
+                                                         nickname: userData.nickname,
+                                                         department: userData.department,
+                                                         studentId: userData.studentId,
+                                                         userRole: userData.userRole,
+                                                         userSelfIntroduction: userData.userSelfIntroduction,
+                                                         profileImage: userData.profileImage))
+        }
+    }
+    
+    private func postWriteTextAPI(joinMemberIDlList: [UserIDList]) {
+        APIPostManager.shared.postWriteText(title: titleContentView.getTitleTextFieldText() ?? "", detail: titleContentView.getContentTextViewText(), joinMemberID: joinMemberIDlList) { response in
+            response.isSuccess ? self.showAlertNavigationBack(message: "게시물 작성이 완료되었습니다.",backType: .pop) : self.errorHandling(response: response)
         }
     }
 }
 
-// MARK: - Delegate
+// MARK: - UITableView DataSource
 
-extension WriteDateBoardVC : UITextViewDelegate{
-    func textViewDidBeginEditing(_ textView: UITextView) {
-           if textView.text == textViewPlaceHolder {
-               textView.text = nil
-               textView.textColor = .black
-               
-           }
-       }
-
-       func textViewDidEndEditing(_ textView: UITextView) {
-           if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-               textView.text = textViewPlaceHolder
-               textView.textColor = UIColor(hexCode: "9F9F9F")
-           }
-           
-       }
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        let currentText = textView.text ?? ""
-        guard let stringRange = Range(range,in: currentText) else { return false}
-        let changedText = currentText.replacingCharacters(in: stringRange, with: text)
-        return changedText.count <= 300
-    }
-}
-
-extension WriteDateBoardVC: UITableViewDelegate {
+extension WriteDateBoardVC: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         2
     }
     
-// MARK: - Cell
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return addMemberDataList.count
-        } else {
-            return 1
-        }
+        return section == 0 ? addMemberDataList.count : 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -168,72 +156,54 @@ extension WriteDateBoardVC: UITableViewDelegate {
             return addCell
         }
     }
+}
+
+// MARK: - Delegate
+
+extension WriteDateBoardVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 {
+            let vc = SearchAddMemberVC()
+            vc.searchAddMemberVCDelegate = self
+            vc.setProperties(pushRequestChatVC: false, addMemberInfos: addMemberDataList)
+            presentViewController(viewController: vc)
+        }
+    }
     
-// MARK: - Header
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: MemberTableViewHeader.identi) as? MemberTableViewHeader else {return UIView()}
-        if section == 0 {
-            headerView.setMemberLabelCount(memberCount: addMemberDataList.count)
-            return headerView
-        }
-        return UIView()
-    }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if section == 1 {
-            return 1
-        }
-        return tableView.estimatedSectionHeaderHeight
-    }
-}
-
-// MARK: - ButtonAction
-
-extension WriteDateBoardVC{
-    private func tapCompletedButton(){
-        var joinMemberID : [UserIDList] = []
+        headerView.setMemberLabelCount(memberCount: addMemberDataList.count)
         
-        for userData in addMemberDataList {
-            joinMemberID.append(UserIDList(id: userData.id))
-        }
-        if joinMemberID.count == 1 {
-            self.showAlert(message: "과팅 게시판 이용은 2명 이상부터 가능합니다.")
-        } else {
-            APIPostManager.shared.postWriteText(title: titleContentView.getTitleTextFieldText() ?? "", detail: titleContentView.getContentTextViewText(), joinMemberID: joinMemberID) { response in
-                if response.isSuccess {
-                    self.showAlertNavigationBack(message: "게시물 작성이 완료되었습니다.",backType: .pop)
-                } else {
-                    self.errorHandling(response: response)
-                }
-            }
-        }
-     
-        
-    }
-}
-extension WriteDateBoardVC{
-    private func getUserData(){
-        APIGetManager.shared.getUserData { userData,response  in
-            self.errorHandling(response: response)
-            guard let userData = userData?.result else { return }
-            self.addMemberDataList.append(UserInfosModel(id: userData.id, name: userData.nickname, gender: userData.gender, age: userData.age, nickname: userData.nickname, department: userData.department, studentId: userData.studentId, userRole: userData.userRole, userSelfIntroduction: userData.userSelfIntroduction, profileImage: userData.profileImage))
-            
-        }
-    }
-}
-
-
-extension WriteDateBoardVC {
-    func sendDetailTextData(textTuple : (String,String)) {
-        titleContentView.setTitleTextFieldText(text: textTuple.0)
-        titleContentView.setContentTextView(text: textTuple.1)
-        titleContentView.setContentTextViewTextColor(color: .black)
+        return section == 0 ? headerView : UIView()
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        section == 1 ? 1 : tableView.estimatedSectionHeaderHeight
+    }
 }
+
+extension WriteDateBoardVC: WriteUpdatePostTextViewDelegate {
+    func tapDoneButton() {
+        view.endEditing(true)
+    }
+}
+
 extension WriteDateBoardVC: SearchAddMemberVCDelegate {
     func sendAddMemberData(send: [UserInfosModel]) {
         addMemberDataList = send
     }
-    
 }
 
+// MARK: - Action
+
+extension WriteDateBoardVC {
+    private func completedButtonAction() {
+        if self.titleContentView.getTitleTextFieldText()?.count == 0 {
+            self.showAlert(message: "제목을 입력해주세요.")
+        } else if self.titleContentView.getContentTextViewText() == Strings.WriteDateBoard.textPlaceHolder {
+            self.showAlert(message: "내용을 입력해주세요.")
+        } else {
+            self.addMemberDataList.count == 1 ? self.showAlert(message: "과팅 게시판 이용은 2명 이상부터 가능합니다.") : postWriteTextAPI(joinMemberIDlList: addMemberID())
+        }
+    }
+}
