84f392a2eb034479a755de10380f1fe0a47c6b5d
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2024-10-31 18:34:31 +0900

    [Chore] #134 -DateBoard/Wirte+Update/Views MemberTableViewHeader - Mark 추가, 코드 정리

--- GNUting/GNUting/Presentation/Main/DateBoard/Wrtie+Update/Views/MemberTableViewHeader.swift
+++ GNUting/GNUting/Presentation/Main/DateBoard/Wrtie+Update/Views/MemberTableViewHeader.swift
@@ -5,20 +5,42 @@
 //  Created by 원동진 on 2/19/24.
 //
 
+// MARK: - 과팅 참가 멤버 TableView Header
+
 import UIKit
 
 class MemberTableViewHeader: UITableViewHeaderFooterView {
+    
+    // MARK: - Properties
+    
     static let identi = "MemberTableViewHeaderoid"
-    private let memberLabel : UILabel = {
+    
+    // MARK: - SubViews
+    
+    private let memberLabel: UILabel = {
         let label = UILabel()
-
-        label.font = Pretendard.medium(size: 18)
         label.textColor = .black
         label.textAlignment = .left
+        label.font = Pretendard.medium(size: 18)
+        
         return label
     }()
+    
+    // MARK: - Init
+    
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Layout Helpers
+    
+    private func configure() {
         contentView.addSubview(memberLabel)
         memberLabel.snp.makeConstraints { make in
             make.top.equalToSuperview()
@@ -27,9 +49,8 @@ class MemberTableViewHeader: UITableViewHeaderFooterView {
         }
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // MARK: - Set View
+    
     func setMemberLabelCount(memberCount : Int){
         memberLabel.text = "멤버(\(memberCount))"
     }
